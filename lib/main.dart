1fe94ce9cd6c34ace7618c4f3d70a9af8b118adc
Tanush Mathur <devspark2501@gmail.com>
2026-08-21 11:14:51 +0530

    Initial commit - Flutter UI

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppCode());
+}
+
+class AppCode extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeCode(),
+    );
+  }
+}
+
+class
