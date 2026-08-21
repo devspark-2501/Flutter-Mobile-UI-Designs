ea9bdfe6c690fa5272901477e2a6d7436597b057
Tanush Mathur <devspark2501@gmail.com>
2026-08-21 14:14:51 +0530

    Planner code file organise

--- lib/Widget/planner.dart
+++ lib/Widget/planner.dart
@@ -3,19 +3,77 @@ import 'package:flutter/material.dart';
 class Planner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Column(
-       crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
 
       children: [
         Text(
           'Your Plan',
-
           style: TextStyle(
             fontSize: 22,
             fontWeight: FontWeight.bold,
           ),
-        )
+        ),
+
+        SizedBox(height: 15),
+
+        Container(
+          width: double.infinity,
+          height: 170,
+
+          padding: EdgeInsets.all(20),
+
+          decoration: BoxDecoration(
+            color: Color(0xFFE8F3E9),
+            borderRadius: BorderRadius.circular(20),
+          ),
+
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              Text(
+                'Yoga Group',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 5),
+
+              Text(
+                'Morning session',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+
+              Spacer(),
+
+              Row(
+                mainAxisAlignment:
+                MainAxisAlignment.spaceBetween,
+
+                children: [
+                  Text(
+                    '08:00 AM',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                  ElevatedButton(
+                    onPressed: () {},
+
+                    child: Text('Start'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
