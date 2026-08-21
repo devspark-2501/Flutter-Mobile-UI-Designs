aea8d1cdf6632fdb0b7a6b73b16250debee1c07a
Tanush Mathur <devspark2501@gmail.com>
2026-08-21 13:53:23 +0530

    dates added according to the date_Selector.dart page

--- lib/Widget/date_selector.dart
+++ lib/Widget/date_selector.dart
@@ -5,7 +5,77 @@ class DateSelector extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Row(
-      children: [],
+      children: [
+        Expanded(
+            child: Column(
+              children: [
+                Text('Sun'),
+                SizedBox(height: 8),
+                Text('22'),
+              ],
+            ),
+        ),
+
+        Expanded(
+            child: Column(
+              children: [
+                Text('Mon'),
+                SizedBox(height: 8),
+                Text('23'),
+              ],
+            ),
+        ),
+
+        Expanded(
+            child: Column(
+              children: [
+                Text('Tue'),
+                SizedBox(height: 8),
+                Text('24'),
+              ],
+            ),
+        ),
+
+        Expanded(
+            child: Column(
+              children: [
+                Text('Wed'),
+                SizedBox(height: 8),
+                Text('25'),
+              ],
+            ),
+        ),
+
+        Expanded(
+            child: Column(
+              children: [
+                Text('Thu'),
+                SizedBox(height: 8),
+                Text('26'),
+              ],
+            ),
+        ),
+
+        Expanded(
+            child: Column(
+              children: [
+                Text('Fri'),
+                SizedBox(height: 8),
+                Text('27'),
+              ],
+            ),
+        ),
+
+        Expanded(
+            child: Column(
+              children: [
+                Text('Sat'),
+                SizedBox(height: 8),
+                Text('28'),
+              ],
+            ),
+        ),
+      ],
     );
   }
 }
