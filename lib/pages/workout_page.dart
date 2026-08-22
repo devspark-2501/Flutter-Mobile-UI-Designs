3c5ac754277e3a5612d8e6d6b898fe21251725d3
Tanush Mathur <devspark2501@gmail.com>
2026-08-22 15:29:11 +0530

    Workout Page, added the starter code

---
+++ lib/pages/workout_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WorkoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Workout Page"),
+      ),
+
+      body: Center(
+        child: Text(
+          "Welcome to Workout Page!!"
+        ),
+      ),
+    );
+  }
+}
