2699261229676fa1f9c0f37d8f8aeafdff03a265
Tanush Mathur <devspark2501@gmail.com>
2026-08-23 23:37:08 +0530

    loading_page state code

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'workout_page.dart';
+
+class LoadingPage extends StatefulWidget {
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+
+}
