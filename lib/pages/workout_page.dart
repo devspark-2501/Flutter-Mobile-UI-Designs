0dc45c0d4e23c2649b1666af3c53cd9f4d705c26
Tanush Mathur <devspark2501@gmail.com>
2026-08-26 20:23:17 +0530

    Workoutpage updated

--- lib/pages/workout_page.dart
+++ lib/pages/workout_page.dart
@@ -9,4 +9,62 @@ class WorkoutPage extends StatefulWidget {
   class _WorkoutPageState extends State<WorkoutPage> {
     TextEditingController exerciseController =
     TextEditingController();
+
+    List<String> exercises = [];
+
+    void addExcercises() {
+      if (exerciseController.text.isNotEmpty) {
+        setState(() {
+          exercises.add(exerciseController.text);
+          exerciseController.clear();
+        });
+      }
+    }
+
+    @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("What did you do today!"),
+      ),
+
+      body: Column(
+        children: [
+          Padding(
+              padding: EdgeInsets.all(20),
+
+            child: Row(
+              children: [
+                Expanded(child: TextField(
+                  controller: exerciseController,
+                  decoration: InputDecoration(
+                    hintText: "Enter Exercise"
+                  ),
+                )),
+                SizedBox(width: 10),
+
+                ElevatedButton(
+                    onPressed: addExcercises,
+                    child: Text("Add"),
+                ),
+              ],
+            ),
+          ),
+
+          Expanded(
+              child: ListView.builder(
+                itemCount: exercises.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(exercises[index],
+                      ),
+                    );
+                }
+            )
+          )
+        ],
+      ),
+    );
+  }
 }
