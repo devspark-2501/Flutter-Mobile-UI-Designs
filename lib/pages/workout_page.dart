6d66816fdd14664a3b0d931767951b16116109f2
Tanush Mathur <devspark2501@gmail.com>
2026-08-27 08:18:04 +0530

    Workout_page merged with timer_widget.dart

--- lib/pages/workout_page.dart
+++ lib/pages/workout_page.dart
@@ -1,69 +1,83 @@
 import 'package:flutter/material.dart';
+import '../Widget/timer_widget.dart';
 
 class WorkoutPage extends StatefulWidget {
   @override
   State<WorkoutPage> createState() => _WorkoutPageState();
+}
 
+class _WorkoutPageState extends State<WorkoutPage> {
+  TextEditingController exerciseController =
+  TextEditingController();
+
+  List<String> exercises = [];
+
+  void addExcercises() {
+    if (exerciseController.text.isNotEmpty) {
+      setState(() {
+        exercises.add(exerciseController.text);
+        exerciseController.clear();
+      });
+    }
   }
 
-  class _WorkoutPageState extends State<WorkoutPage> {
-    TextEditingController exerciseController =
-    TextEditingController();
-
-    List<String> exercises = [];
-
-    void addExcercises() {
-      if (exerciseController.text.isNotEmpty) {
-        setState(() {
-          exercises.add(exerciseController.text);
-          exerciseController.clear();
-        });
-      }
-    }
-
-    @override
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text("What did you do today!"),
+
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+
+            child: Center(
+              child: TimerWidget(),
+            ),
+          ),
+        ],
       ),
 
       body: Column(
         children: [
-
           Padding(
-              padding: EdgeInsets.all(20),
+            padding: EdgeInsets.all(20),
 
             child: Row(
               children: [
-                Expanded(child: TextField(
-                  controller: exerciseController,
-                  decoration: InputDecoration(
-                    hintText: "Enter Exercise"
+                Expanded(
+                  child: TextField(
+                    controller: exerciseController,
+
+                    decoration: InputDecoration(
+                      hintText: "Enter Exercise",
+                    ),
                   ),
-                )),
+                ),
+
                 SizedBox(width: 10),
 
                 ElevatedButton(
-                    onPressed: addExcercises,
-                    child: Text("Add"),
+                  onPressed: addExcercises,
+                  child: Text("Add"),
                 ),
               ],
             ),
           ),
 
           Expanded(
-              child: ListView.builder(
-                itemCount: exercises.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(exercises[index],
-                      ),
-                    );
-                }
-            )
-          )
+            child: ListView.builder(
+              itemCount: exercises.length,
+
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    exercises[index],
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
