ed7eb7cc12379559a6d913680cd681bb529d277a
Tanush Mathur <devspark2501@gmail.com>
2026-08-27 07:33:02 +0530

    Timer_Widget.dart created, to create instance of an digital clock

---
+++ lib/Widget/timer_widget.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class TimerWidget extends StatefulWidget {
+  @override
+  State<TimerWidget> createState() => _TimerWidgetState();
+  }
+
+  class _TimerWidgetState extends State<TimerWidget> {
+
+  }
+}
